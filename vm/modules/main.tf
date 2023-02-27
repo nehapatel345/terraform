ba00af1e6bfb1058c6660ebd7d21a2122761ca33
nehapatel345 <99860862+nehapatel345@users.noreply.github.com>
2023-02-27 19:37:10 +0530

    Create main.tf

---
+++ vm/modules/main.tf
@@ -0,0 +1,32 @@
+resource "google_service_account" "default" {
+  account_id   = var.service_account_id
+  display_name = var.service_account_name
+}
+
+resource "google_compute_instance" "vm" {
+  name                      = var.name
+  machine_type              = var.machine
+  zone                      = var.zone
+  tags                      = [var.tags]
+  allow_stopping_for_update = var.allow_stopping_for_update
+  deletion_protection       = var.deletion_protection
+
+  boot_disk {
+    initialize_params {
+      image = var.disk_image
+    }
+  }
+
+  network_interface {
+    network = var.vpc_network
+  }
+
+ // scratch_disk {
+   // interface = var.scratch_disk
+  //}
+  service_account {
+    email  = google_service_account.default.email
+    scopes = [var.scope]
+  }
+}
+
